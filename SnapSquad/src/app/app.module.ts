4096a4c2496e2daeb847ea335eb069debd27ccb7
Matt Marano <mattmarano@MMARANO-MBP-LA.local>
2019-06-12 15:24:05 -0500

    Imported Forms Module for two way component communication

--- SnapSquad/src/app/app.module.ts
+++ SnapSquad/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,6 +24,7 @@ import { EventPageComponent } from './event-page/event-page.component'
     EventPageComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
