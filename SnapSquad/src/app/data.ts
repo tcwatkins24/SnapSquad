e723db7fa2006e25d6cadda1ef6893b92f9cb701
Matt Marano <mattmarano@MMARANO-MBP-LA.local>
2019-06-13 13:34:17 -0500

    Changed some dates in data.ts to make them created in may

--- SnapSquad/src/app/data.ts
+++ SnapSquad/src/app/data.ts
@@ -4,21 +4,21 @@ export var events = [
         summary: "Going to Deep Ellum Brewing!",
         pictureName: "Cool Brewery",
         reasonsToGo: ["Fun", "Cheap", "Good time!"],
-        dateCreated: new Date('June 5, 2019 08:24:00')
+        dateCreated: new Date('May 4, 2019 08:24:00')
     },
     {
         name: "Bar Tour 2", date: new Date('June 3, 2019 08:24:00'),
         summary: "Going to Deep Ellum Brewing!",
         pictureName: "Cool Brewery",
         reasonsToGo: ["Fun", "Cheap", "Good time!"],
-        dateCreated: new Date('June 3, 2019 08:24:00')
+        dateCreated: new Date('May 3, 2019 08:24:00')
     },
     {
         name: "Bar Tour 3", date: new Date('June 4, 2019 08:24:00'),
         summary: "Going to Deep Ellum Brewing!",
         pictureName: "Cool Brewery",
         reasonsToGo: ["Fun", "Cheap", "Good time!"],
-        dateCreated: new Date('June 1, 2019 08:24:00')
+        dateCreated: new Date('May 2, 2019 08:24:00')
     }
 ];
 
