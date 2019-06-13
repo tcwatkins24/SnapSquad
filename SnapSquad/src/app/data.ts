25fadd629dd8b0bc6d1822f89cf354949b794b08
Matt Marano <mattmarano@MMARANO-MBP-LA.local>
2019-06-13 11:51:55 -0500

    Added data.ts

--- SnapSquad/src/app/data.ts
+++ SnapSquad/src/app/data.ts
@@ -1,8 +1,25 @@
 export var events = [
-    {name: "Bar Tour", date: "June 15th", summary: "Going to Deep Ellum Brewing!", pictureName: "Cool Brewery", reasonsToGo: ["Fun", "Cheap", "Good time!"]},
-    {name: "Bar Tour", date: "June 15th", summary: "Going to Deep Ellum Brewing!", pictureName: "Cool Brewery", reasonsToGo: ["Fun", "Cheap", "Good time!"]},
-    {name: "Bar Tour", date: "June 15th", summary: "Going to Deep Ellum Brewing!", pictureName: "Cool Brewery", reasonsToGo: ["Fun", "Cheap", "Good time!"]}
-    
-    
-    
+    {
+        name: "Bar Tour", date: new Date('June 2, 2019 08:24:00'),
+        summary: "Going to Deep Ellum Brewing!",
+        pictureName: "Cool Brewery",
+        reasonsToGo: ["Fun", "Cheap", "Good time!"],
+        dateCreated: new Date('June 5, 2019 08:24:00')
+    },
+    {
+        name: "Bar Tour 2", date: new Date('June 3, 2019 08:24:00'),
+        summary: "Going to Deep Ellum Brewing!",
+        pictureName: "Cool Brewery",
+        reasonsToGo: ["Fun", "Cheap", "Good time!"],
+        dateCreated: new Date('June 3, 2019 08:24:00')
+    },
+    {
+        name: "Bar Tour 3", date: new Date('June 4, 2019 08:24:00'),
+        summary: "Going to Deep Ellum Brewing!",
+        pictureName: "Cool Brewery",
+        reasonsToGo: ["Fun", "Cheap", "Good time!"],
+        dateCreated: new Date('June 1, 2019 08:24:00')
+    }
 ];
+
+export var currentEventName = "Bar Tour";
